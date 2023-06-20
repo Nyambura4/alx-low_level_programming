c73877d798d18cd77bafbc207b12778e0d2f4d65
Nyambura4 <marryannfrancis4520@gmail.com>
2023-06-20 13:09:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
-/* main-print putchar
- * use putchar instead of printf
- * a program that prints putchar followed by a new line
- */
 
+/**
+ * main - Entry point
+ * Description: A program that prints putchar followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('C');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    
-    return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('C');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
+
