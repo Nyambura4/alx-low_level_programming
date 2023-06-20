ecedec0a9c1bad884a04f77c16cef3b8fb868b5e
Nyambura4 <marryannfrancis4520@gmail.com>
2023-06-20 13:12:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Description: A program that prints putchar followed by a new line
+ * Description: A program that prints _putchar followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
