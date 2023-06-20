919ee49bdfd634c7414249e8b52c58e050e2bbeb
Nyambura4 <marryannfrancis4520@gmail.com>
2023-06-20 14:30:54 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabets in lowercase
+ *
+ * Description: This function prints alphabets from 'a' to 'z'
+ *              using the _putchar function.
+ */
+void print_alphabet(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        _putchar(c);
+    }
+
+    _putchar('\n');
+}
+
