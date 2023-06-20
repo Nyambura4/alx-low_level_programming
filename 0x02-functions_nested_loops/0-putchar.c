29f257f8f065edcb30f34aef7a5e7320ccaca394
Nyambura4 <marryannfrancis4520@gmail.com>
2023-06-20 13:03:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+/* main-print putchar
+ * use putchar instead of printf
+ * a program that prints putchar followed by a new line
+ */
+
 int main(void)
 {
-	printf("putchar \n");
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('C');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    
+    return 0;
 }
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+/**
+ * Writes the character c to stdout
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
