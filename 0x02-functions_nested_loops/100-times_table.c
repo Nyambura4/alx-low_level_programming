6aba6b7cc1bd2344a24057dcabe907ef9d866622
Nyambura4 <marryannfrancis4520@gmail.com>
2023-06-20 18:56:12 -0700

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void print_times_table(int n) 
+{
+int i, j;
+if (n < 0 || n > 15)
+{
+return;
+}
+for (i = 0; i <= n; i++) 
+{
+for (j = 0; j <= n; j++) 
+{
+printf("%4d", i * j);
+}
+printf("\n");
+}
+}
