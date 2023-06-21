a0a9130b69e0de1b1df55eb1d5c23df2eb5e60a0
Nyambura4 <marryannfrancis4520@gmail.com>
2023-06-20 19:05:59 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-void print_times_table(int n) 
-{
-int i, j;
-if (n < 0 || n > 15)
-{
-return;
+/**
+ * Prints the times table up to a given number.
+ *
+ * @param n The number to generate the times table for.
+ */
+void print_times_table(int n) {
+// Check if n is within the valid range
+if (n < 0 || n > 15) {
+return;  // Exit the function without printing anything
 }
-for (i = 0; i <= n; i++) 
-{
-for (j = 0; j <= n; j++) 
-{
-printf("%4d", i * j);
+// Print the times table
+for (int i = 0; i <= n; i++) {
+for (int j = 0; j <= n; j++) {
+int product = i * j;
+if (j > 0) {
+printf(",   "); // Adjust spacing
+}
+printf("%3d", product); // Print with 3-digit spacing
 }
 printf("\n");
 }
