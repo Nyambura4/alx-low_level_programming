392604de92becb9e8bc0b61aee7647fde324a85b
Nyambura4 <marryannfrancis4520@gmail.com>
2023-06-22 14:16:20 -0700

    one main c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/* while (i < 10) */
+putchar(i);
+}
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
